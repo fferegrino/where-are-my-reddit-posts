4837666837ecb3e387121f941596289060b48dfa
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-10-08 16:50:41 +0100

    Examine comment forest object.

--- WhereAreMyRedditPosts.py
+++ WhereAreMyRedditPosts.py
@@ -9,7 +9,7 @@ password = ""
 username = ""
 
 start_date = datetime.date.today()
-end_date = datetime.date(2017, 10, 5)
+end_date = datetime.date(2017, 10, 6)
 time_delay = datetime.timedelta(days=1)
 
 reddit = praw.Reddit(client_id=client_id,
@@ -19,6 +19,8 @@ reddit = praw.Reddit(client_id=client_id,
                      username=username)
 
 rbitcoin = reddit.subreddit('bitcoin');
+
+comment_forest = None
 
 with open('reddit-posts.csv', 'w', encoding='utf-8') as reddit_posts_csv:
     spamwriter = csv.writer(reddit_posts_csv, delimiter=',',
@@ -33,9 +35,13 @@ with open('reddit-posts.csv', 'w', encoding='utf-8') as reddit_posts_csv:
         q = "(and timestamp:{}..{})".format(start_timestamp, end_timestamp)
         relevant_bitcoin_posts = rbitcoin.search(q, syntax='cloudsearch', limit=10)
         for submission in relevant_bitcoin_posts:
-            print(submission.title)
-            spamwriter.writerow([int(submission.created_utc),
-                                 submission.title])
+            if comment_forest == None:
+                comment_forest = submission.comments
+            spamwriter.writerow([submission.id,
+                                 int(submission.created_utc),
+                                 submission.title,
+                                 submission.url])
 
         start_date = start_date - time_delay
         time.sleep(5)
+dir(comment_forest)
