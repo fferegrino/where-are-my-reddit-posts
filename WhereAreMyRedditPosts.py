59057f8d1cc79385461e7a0c366f9452b9474fa6
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-10-11 08:16:51 +0100

    Grab more information about the submission.

--- WhereAreMyRedditPosts.py
+++ WhereAreMyRedditPosts.py
@@ -6,10 +6,10 @@ import csv
 client_id = ""
 client_secret = ""
 password = ""
-username = "∫"
+username = ""
 
-start_date = datetime.date.today()
-end_date = datetime.date(2017, 10, 6)
+start_date = datetime.date(2017, 10, 12) # Un dia más de lo deseado
+end_date = datetime.date(2017, 10, 9)
 time_delay = datetime.timedelta(days=1)
 
 reddit = praw.Reddit(client_id=client_id,
@@ -18,11 +18,13 @@ reddit = praw.Reddit(client_id=client_id,
                      user_agent='praw_playground by /u/' + username,
                      username=username)
 
-rbitcoin = reddit.subreddit('bitcoin');
+rbitcoin = reddit.subreddit('worldnews');
 
 with open('reddit-posts.csv', 'w', encoding='utf-8') as reddit_posts_csv:
     submwriter = csv.writer(reddit_posts_csv, delimiter=',',
                             quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    
+    submwriter.writerow(["id", "created_utc", "score","title","url","permalink","comments"])
     while start_date > end_date:
         temp_date = start_date - time_delay
         start_timestamp = int(time.mktime(temp_date.timetuple()))
@@ -31,12 +33,15 @@ with open('reddit-posts.csv', 'w', encoding='utf-8') as reddit_posts_csv:
         print("From ", start_date , "to", temp_date)
 
         q = "(and timestamp:{}..{})".format(start_timestamp, end_timestamp)
-        relevant_bitcoin_posts = rbitcoin.search(q, syntax='cloudsearch', limit=10)
-        for submission in relevant_bitcoin_posts:
+        relevant_posts = rbitcoin.search(q, syntax='cloudsearch', limit=20)
+        for submission in relevant_posts:
             submwriter.writerow([submission.id,
                                 int(submission.created_utc),
+                                int(submission.score),
                                 submission.title,
-                                submission.url])
+                                submission.url,
+                                submission.permalink,
+                                int(submission.num_comments)])
 
         start_date = start_date - time_delay
         time.sleep(5)
