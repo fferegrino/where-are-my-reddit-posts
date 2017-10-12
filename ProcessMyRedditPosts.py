39c4d592b5689f9bbdfee5c5b5901b3aad1825e8
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-10-12 10:40:30 +0100

    Gathering the news

--- ProcessMyRedditPosts.py
+++ ProcessMyRedditPosts.py
@@ -1,8 +1,11 @@
 import praw
 import csv
 import json
+import unicodedata 
+import datetime
 from RedditObjects.Submission import Submission
 from RedditObjects.Comment import Comment
+from newsplease import NewsPlease
 
 client_id = ""
 client_secret = ""
@@ -26,7 +29,13 @@ def process_submission(submission_id):
     submission.comments.replace_more(limit=0)
     comment_queue = []
     tab_queue = []
-    sub = Submission(submission_id, submission.title, submission.url)
+    sub = Submission(submission_id, 
+                     unicodedata.normalize("NFKD", submission.title), 
+                     submission.url,
+                     int(submission.created_utc),
+                     submission.ups,
+                     submission.downs,
+                     submission.score)
     for s in submission.comments:
         comment_queue.append(s)
         tab_queue.append(0)
@@ -36,28 +45,42 @@ def process_submission(submission_id):
         com = Comment(
                       comment.id, 
                       comment.parent_id, 
+                      unicodedata.normalize("NFKD", comment.body),
+                      tab,
+                      int(comment.created_utc),
                       comment.ups,
-                      comment.body,
-                      tab)
+                      comment.downs,
+                      comment.score)
         sub.comments.append(com)
-        # print("\t" * tab,tab,comment.body.encode('utf-8'))
+        if tab > 4:
+            continue
         for reply in comment.replies:
             comment_queue.append(reply)
             tab_queue.append(tab+1)
     return sub
 
-number_of_rows = 1
-submissions = []
+number_of_rows = 1000
 
-with open('reddit-posts.csv', 'r', encoding='utf-8') as csvfile:
+with open('data/reddit-posts-2017-10-12-2011-12-31-fixed.csv', 'r', encoding='utf-8') as csvfile:
     i = 0
     post_reader = csv.reader(csvfile, delimiter=',',
                             quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    wrong_posts = []
+    next(post_reader)
     for row in post_reader:
+        i = i + 1
         if i > number_of_rows:
             break
-        i = i + 1
-        submissions.append(process_submission(row[0]))
-
-#print(json.dumps(submissions, cls=MyEncoder))
-
+        try:
+            sub = process_submission(row[0])
+            article = NewsPlease.from_url(row[4])
+            sub.actual_title = unicodedata.normalize("NFKD", article.title)
+            sub.news_text = unicodedata.normalize("NFKD", article.text)
+            date = datetime.datetime.fromtimestamp(sub.created_utc).strftime('%Y-%m-%d')
+            f = open("data/submissions/"+ date + "-" + row[0] + ".json", 'w')
+            f.write(json.dumps(sub, cls=MyEncoder))
+        except:
+            print("Something went wrong: ", row[0])
+            wrong_posts.append(row[0])
+    fwrong = open("data/wrong.json", 'w')
+    fwrong.write(json.dumps(wrong_posts))
