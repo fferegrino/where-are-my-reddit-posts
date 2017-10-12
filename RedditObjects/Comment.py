4d10588ef4dedb332dd176ef916181fc43cb708b
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-10-12 10:16:03 +0100

    Added features

--- RedditObjects/Comment.py
+++ RedditObjects/Comment.py
@@ -1,8 +1,11 @@
 class Comment(object):
     
-    def __init__(self, id, in_reply_to, ups, body, comment_level):
+    def __init__(self, id, in_reply_to, body, comment_level, created_utc, ups, downs, score):
         self.id = id
         self.ups = ups
+        self.downs = downs
+        self.score = score
         self.body = body
+        self.created_utc = created_utc
         self.in_reply_to = in_reply_to
         self.comment_level = comment_level
--- RedditObjects/Submission.py
+++ RedditObjects/Submission.py
@@ -1,7 +1,13 @@
 class Submission(object):
     
-    def __init__(self, sub_id, title, url):
+    def __init__(self, sub_id, title, url, created_utc, ups, downs, score):
         self.id = sub_id
         self.title = title
+        self.news_text = None
+        self.actual_title = None
+        self.created_utc = created_utc
+        self.ups = ups
+        self.downs = downs
+        self.score = score
         self.url = url
         self.comments = []
