6aeeb9d8b8e888562ce28116791c4b568c348bf9
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-10-19 07:51:30 +0100

    textprocessing tool

---
+++ textprocessing.py
@@ -0,0 +1,92 @@
+import requests # http://docs.python-requests.org/en/master/user/quickstart/#make-a-request
+import json
+
+import sys
+
+import os
+from os import listdir
+import os.path
+from os.path import isfile, join
+
+from processor.simple_encoder import SimpleEncoder
+
+import json
+import csv
+
+mashape_key = ""
+
+def sentiment_analysis(text):
+    # http://text-processing.com/docs/sentiment.html
+    url = 'https://japerk-text-processing.p.mashape.com/sentiment/'
+    headers = {
+        "X-Mashape-Key": mashape_key,
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "application/json"
+    }
+
+    payload = {
+        "language": "english",
+        "text": text
+    }
+    request = requests.post(url, headers=headers, data=payload)
+    return request.json()
+
+def tagging(text):
+    # http://text-processing.com/docs/tag.html
+    url = 'https://japerk-text-processing.p.mashape.com/tag/'
+    headers = {
+        "X-Mashape-Key": mashape_key,
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "application/json"
+    }
+
+    payload ={
+    "language": "english",
+    "output": "sexpr",
+    "text": text
+  }
+    request = requests.post(url, headers=headers, data=payload)
+    return request.json()
+
+
+input_data_folder= ""
+output_data_folder =""
+json_files_to_analyze = [f for f in listdir(input_data_folder) if isfile(join(input_data_folder, f)) and f.endswith(".json")]
+
+for json_file in json_files_to_analyze:
+    print("Processing", json_file)
+    with open(join(input_data_folder, json_file)) as data_file:
+        data = json.load(data_file)
+
+    sentiments = sentiment_analysis(data["news_text"][:80000])
+    tags = tagging(data["news_text"][:2000])
+
+    processed = {
+        "identified_as": sentiments["label"],
+        "sentiments" : sentiments["probability"],
+        "tagged_text" : tags["text"],
+        "actual_text" : data["news_text"]
+    }
+
+    comments = []
+    max_comment_count = 20
+    ii = 0
+    for i, comment in enumerate(data["comments"]):
+        if ii > max_comment_count:
+            break
+        if len(comment["body"]) < 40:
+            continue
+
+        ii = ii + 1
+        comment_sentiments = sentiment_analysis(comment["body"][:80000])
+        comment_analysis = {
+            "identified_as": comment_sentiments["label"],
+            "sentiments" : comment_sentiments["probability"],
+            "text": comment["body"]
+        }
+        comments.append(comment_analysis)
+    processed["comments"] = comments
+
+    with open(output_data_folder + json_file, 'w', encoding='utf-8') as submission_file:
+        submission_file.write(json.dumps(processed, indent=4, cls=SimpleEncoder))
+
