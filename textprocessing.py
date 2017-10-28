f71dff9bae775841e00654d9bafa938f1e9c8eee
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-10-28 06:38:31 +0100

    Consider yearly submissions

--- textprocessing.py
+++ textprocessing.py
@@ -1,9 +1,8 @@
 import json
 import csv
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, isdir
 
-from processor.simple_encoder import SimpleEncoder
 from analizers.vader import vader_sentiment
 from taggers.keyphrase import keyphrases_extractor
 
@@ -17,39 +16,49 @@ class Row():
         self.compound = compound
         self.keyphrases = keyphrases
 
-input_data_folder = "data"
+year = "2017"
+
+input_data_folder = r""
 output_data_folder = ""
-json_files_to_analyze = [f for f in listdir(input_data_folder) if
-                         isfile(join(input_data_folder, f)) and f.endswith(".json")]
-
+folders = [d for d in listdir(input_data_folder) if isdir(join(input_data_folder,d))]
 rows = []
-for json_file in json_files_to_analyze:
-    print("Processing", json_file)
-    with open(join(input_data_folder, json_file)) as data_file:
-        data = json.load(data_file)
 
-    news_text = data["news_text"]
+for folder in folders:
+    current_folder = join(input_data_folder,folder)
+    print(current_folder)
+    json_files_to_analyze = [f for f in listdir(current_folder) if
+                             isfile(join(current_folder, f)) and f.endswith(".json")]
 
-    sentiments = vader_sentiment(data["news_text"])
+    for json_file in json_files_to_analyze:
+        current_file = join(folder, json_file)
+        print(current_file)
 
-    keyphrases = keyphrases_extractor(data["news_text"],3)
+        with open(current_file) as data_file:
+            data = json.load(data_file)
 
-    news_row = Row(1, data["created_utc"], sentiments["pos"], sentiments["neg"], sentiments["neu"],
-                   sentiments["compound"], keyphrases)
 
-    rows.append(news_row)
-    for i, comment in enumerate(data["comments"]):
-        body = comment["body"]
-        comment_sentiments = vader_sentiment(body)
+        news_text = data["news_text"]
 
-        keyphrases = keyphrases_extractor(body,3)
+        sentiments = vader_sentiment(data["news_text"])
 
-        comment_row = Row(2, comment["created_utc"], comment_sentiments["pos"], comment_sentiments["neg"],
-                          comment_sentiments["neu"], sentiments["compound"], keyphrases)
+        keyphrases = keyphrases_extractor(data["news_text"],3)
 
-        rows.append(comment_row)
+        news_row = Row(1, data["created_utc"], sentiments["pos"], sentiments["neg"], sentiments["neu"],
+                       sentiments["compound"], keyphrases)
 
-with open("results.csv", 'w', encoding='utf8', newline='') as results_csv:
+        rows.append(news_row)
+        for i, comment in enumerate(data["comments"]):
+            body = comment["body"]
+            comment_sentiments = vader_sentiment(body)
+
+            keyphrases = keyphrases_extractor(body,3)
+
+            comment_row = Row(2, comment["created_utc"], comment_sentiments["pos"], comment_sentiments["neg"],
+                              comment_sentiments["neu"], sentiments["compound"], keyphrases)
+
+            rows.append(comment_row)
+
+with open("r" + year + ".csv", 'w', encoding='utf8', newline='') as results_csv:
     submwriter = csv.writer(results_csv, delimiter=',',
                             quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for row in rows:
